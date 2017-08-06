92a4ef534a11f65e73ac08c71c50e411b85f8521
Vlad Petrov <ejiek@mail.ru>
2017-08-06 13:41:27 +0300

    add modem status to bar

---
+++ i3/.config/i3/scripts/modem.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+# red cc241d
+# green 8fd989
+ENABLED=$(mmcli -m 0 | grep 'power state' | awk -F"'" '{ print $2 }')
+if [[ $ENABLED -eq "on" ]]; then
+	MODEM=$(mmcli -L | tail -n2 | head -n 1 | awk -F"/" '{ print $6 }' | awk '{ print $1 }')
+	SIGNAL=$(mmcli -m $MODEM | grep quality | awk -F"'" '{ print $2 }')
+	TECH=$(mmcli -m $MODEM | grep tech | awk -F"'" '{ print $2 }')
+
+	RED=$( echo $((204 - 0.61*$SIGNAL)) | xargs printf '%X\n' | awk '{printf "%2s\n", $0}' | sed 's/ /0/g')
+	GREEN=$( echo $((36 + 1.81*$SIGNAL)) | xargs printf '%X\n'| awk '{printf "%2s\n", $0}' | sed 's/ /0/g')
+	BLUE=$( echo $((29 + 1.08*$SIGNAL)) | xargs printf '%X\n'| awk '{printf "%2s\n", $0}' | sed 's/ /0/g')
+	COLOUR="'#$RED$GREEN$BLUE'"
+
+	echo -ne "<span color=$COLOUR> \uf012 </span> $TECH"
+else
+	echo -ne "<span color='#fe8019'> \uf012 </span> off"
+fi
