abc763c75f6eb69bfd748e4fb0d33aee340d7fc5
Vlad Petrov <ejiek@mail.ru>
2017-08-06 16:10:47 +0300

    fix modem IF condition and colour

--- i3/.config/i3/scripts/modem.sh
+++ i3/.config/i3/scripts/modem.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 # red cc241d
 # green 8fd989
-ENABLED=$(mmcli -m 0 | grep 'power state' | awk -F"'" '{ print $2 }')
-if [[ $ENABLED -eq "on" ]]; then
+ENABLED=$(mmcli -m 0 | grep 'state' | head -n1 | awk -F"'" '{ print $2 }')
+if [[ $ENABLED != "disabled" ]]; then
 	MODEM=$(mmcli -L | tail -n2 | head -n 1 | awk -F"/" '{ print $6 }' | awk '{ print $1 }')
 	SIGNAL=$(mmcli -m $MODEM | grep quality | awk -F"'" '{ print $2 }')
 	TECH=$(mmcli -m $MODEM | grep tech | awk -F"'" '{ print $2 }')
@@ -14,5 +14,5 @@ if [[ $ENABLED -eq "on" ]]; then
 
 	echo -ne "<span color=$COLOUR> \uf012 </span> $TECH"
 else
-	echo -ne "<span color='#fe8019'> \uf012 </span> off"
+	echo -ne "<span color='#3c3836'> \uf012 </span> off"
 fi
