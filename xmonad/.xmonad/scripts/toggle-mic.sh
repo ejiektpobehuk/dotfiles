0e1364a7b6603de30dff5537d73d0db6b1176741
Vlad Petrov <ejiek@mail.ru>
2020-07-09 17:09:14 +0300

    [xmonad] upd mic toggle script

--- xmonad/.xmonad/scripts/toggle-mic.sh
+++ xmonad/.xmonad/scripts/toggle-mic.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env sh
+
+# Toggle mic muteness and shows notification with new status
+
 DEFAULT=$(pactl info | grep "Default Source" | cut -d " " -f3)
 pactl set-source-mute "$DEFAULT" toggle
+MUTE=$(pactl list | grep -A53 "$DEFAULT" | grep Mute | awk '{ print $2 }')
+if [ "$MUTE" = "yes" ]; then
+  dunstify -I /usr/share/icons/Arc/status/symbolic/microphone-sensitivity-muted-symbolic.svg " " "Mute" -r 2682 -u critical
+else
+  dunstify -I /usr/share/icons/Arc/status/symbolic/microphone-sensitivity-high-symbolic.svg " " "Unmute" -r 2682 -u critical
+fi;
